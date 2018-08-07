ce5318f2d4341e08f91c77d9db230fbd8b61eb55
Vasia Danilyuk <38600598+Nvgdsk@users.noreply.github.com>
2018-08-07 20:04:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckPrime check = new CheckPrime();
+            int numb;
+            while (true)
+            {
+                
+                Console.WriteLine("Input number: ");
+                numb = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(check.Finalcheck(check.CheckNumb(numb),numb));
+            }
+        }
+    }
+}
